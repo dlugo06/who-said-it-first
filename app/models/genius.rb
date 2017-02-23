c4a074e3780ef57270dea5d3f9e33cc26fde11bd
Diego Lugo <diego.lugo.06@gmail.com>
2017-02-22 20:11:06 -0800

    Genius auth token

--- app/models/genius.rb
+++ app/models/genius.rb
@@ -1,2 +1,15 @@
 class Genius < ApplicationRecord
+  def self.get_auth_token
+    client_id = ENV['CLIENT_ID']
+    client_secret = ENV['CLIENT_SECRET']
+    client = OAuth2::Client.new(client_id, client_secret, site: "https://api.genius.com/oauth/authorize")
+    url = client.auth_code.authorize_url
+    binding.pry
+
+    client.auth_code.get_token(url)
+  end
+
+  def self.get_annotation(token)
+    token.get('api.genius.com/annotations/10225840', :params => { 'query_foo' => 'bar' })
+  end
 end
