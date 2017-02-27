5846cf8fd1864a5b26b374b549b83e693e0e30a1
Diego Lugo <diego.lugo.06@gmail.com>
2017-02-26 20:58:36 -0800

    display search results

--- app/controllers/musixmatches_controller.rb
+++ app/controllers/musixmatches_controller.rb
@@ -7,7 +7,20 @@ class MusixmatchesController < ApplicationController
 
   def search
     @result = Musixmatch.search_get(musix_params)
-    render json: @result
+    @musix_results = []
+    @result['message']['body']['track_list'].each do |track|
+      track = track['track']
+      @musix = Musixmatch.new
+      unless track['first_release_date'].blank?
+        @musix.release_date = track['first_release_date'].to_date
+        @musix.song_name = track['track_name']
+        @musix.album_name = track['album_name']
+        @musix.artist_name = track['artist_name']
+        @musix_results << @musix
+      end
+    end
+
+    @musix_results# = Musixmatch.where(id: @musix_results.map(&:id))
   end
 
   private
